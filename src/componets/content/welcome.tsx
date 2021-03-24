041f8f93f3ecf618acfe49e4e453530c47e90b80
Sleepingtree <ajgrabow@gmail.com>
2021-03-23 21:13:56 -0400

    Cleaned up some code, getting there for layout

--- src/componets/content/welcome.tsx
+++ src/componets/content/welcome.tsx
@@ -1,18 +1,23 @@
 import { FunctionComponent } from "react";
+import { Container, Image } from 'semantic-ui-react';
+
+import banner from '../../resorces/banner-2.png'
+import avatarImage from '../../resorces/avatar 1.png'
 
 type WelcomeProps = {
   inverted?: boolean
 }
 
-const Welcome: FunctionComponent<WelcomeProps> = ({inverted = true}) => {
-    return (
-      <div>
-        <body className={inverted? 'dark': ''}>
-          this is a Welcome page
-        </body>
-      </div>
-    )
-  }
-  
-  export default Welcome;
-  
+const Welcome: FunctionComponent<WelcomeProps> = ({ inverted = true }) =>
+  <div>
+    <Container>
+      <Image src={avatarImage} avatar size='small' floated ='left'/>
+      <Container text right floated='right'>
+        This site was made with TypeScript, React and Simantitic UI. The perpose of this site it to serve as a mixed personal and profesinal site. 
+        I consider myself more of a backend devloper then front end, but hopefully that doesn't show to much.
+      </Container>
+      <Image src={banner} centered />
+    </Container>
+  </div>
+
+export default Welcome;
--- src/componets/layout/footer.tsx
+++ src/componets/layout/footer.tsx
@@ -1,30 +1,32 @@
 import React, { FunctionComponent } from "react";
-import { Container, Grid, Header, Segment } from "semantic-ui-react";
+import { Container, Grid, Header, Segment, Sticky } from "semantic-ui-react";
 
 type FooterProps = {
-    inverted?: boolean
+  inverted?: boolean
 }
 
-const Footer: FunctionComponent<FooterProps> = ({inverted = true}) => 
-    <aside>
-        <Segment inverted ={inverted} vertical style={{ padding: '5em 0em' }}>
-        <Container>
-          <Grid divided inverted stackable>
-            <Grid.Row>
-              <Grid.Column width={10}>
-                <Header as='h4' inverted>
-                  Contact Info
+const Footer: FunctionComponent<FooterProps> = ({ inverted = true }) =>
+  <Sticky className='ui fixed bottom sticky' style={{ width: '100%' }}>
+    <Segment inverted={inverted} vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted={inverted} stackable>
+          <Grid.Row>
+            <Grid.Column width={5} />
+            <Grid.Column width={5}>
+              <Header as='h4' inverted={inverted}>
+                Contact Info
                 </Header>
-                <p>
-                  Email: <a href ='mailto:ajgrabow@gmail.com'>ajgrabow@gmail.com</a>
-                  <br/>
+              <Container text>
+                Email: <a href='mailto:ajgrabow@gmail.com'>ajgrabow@gmail.com</a>
+                <br />
                   GitHub: <a href='https://github.com/Sleepingtree'>https://github.com/Sleepingtree</a>
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </Segment>
-  </aside>
-  
-  export default Footer;
+              </Container>
+            </Grid.Column>
+            <Grid.Column width={5} />
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+  </Sticky>
+
+export default Footer;
