8c3f782c1a4a066e9cd863302e4a893928746adc
Sleepingtree <ajgrabow@gmail.com>
2021-03-29 23:02:35 -0400

    more clean up and set up for bot

--- src/paths.ts
+++ src/paths.ts
@@ -1,4 +1,6 @@
-export const baseUrl = process.env.BASE_PATH_URL ?? "/";
+export const baseUrl = "/";
+
+export const dsiscordBotUrl = process.env.REACT_APP_DISCORD_BOT_URL ?? "localhost:3001";
 
 export const projectsPath = `${baseUrl}projects`;
 export const discordBotPath = `${projectsPath}/discordBot`;
