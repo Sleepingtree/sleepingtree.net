ddcee1146b2bdeb5e66140e55b6084916a9afe89
Garry Grabow <glgrabow@ncsu.edu>
2021-04-28 11:12:38 -0400

    edits for grammar

--- src/componets/content/discordBot.tsx
+++ src/componets/content/discordBot.tsx
@@ -37,8 +37,8 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
               a React app as a proof of concept for modernizing our internal UI. They have used React before and already prototyped a skeleton UI.
               <Divider hidden />
   
-              However; as many UI's go the actual UI is only half the battle. I was brought on to help with the backend implimentation. In retrospect
-              My boss probably assumed I would implement this backend with Java, as we were a primarly a Java based at the time, I was talked into
+              However; as many UI's go the actual UI is only half the battle. I was brought on to help with the backend implementation. In retrospect
+              My boss probably assumed I would implement this backend with Java, as we were a primarly Java based at the time, I was talked into
               implementing the back end in Node.js to keep the stack homogeneous.
               <Divider hidden />
   
@@ -47,10 +47,10 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
         </Container>
         {isMobile && ref ? MobileDiscordBotSection() : <Divider hidden />}
         <Container text inverted={context.inverted}>
-          Around the same time Valerant was the new hotness as far as gaming went, and I was part of a large enough group that we could consitently
-          have our own in house games. In the past I had built a bot to help with in house games, but used for league. It used a system where people could list
-          their prefered roles and try and figure out how to make teams. This bot had some issues with it which I will get to later. With all this in mind
-          I decided to kill two birds with one stone so to speak. Build a Bot to learn Node and help us spead up in house game creation.
+          Around the same time Valerant was the new hotness as far as gaming went, and I was part of a large enough group that we could consistently
+          have our own in-house games. In the past I had built a bot to help with in-house games, but used for league. It used a system in which people could list
+          their preferred roles and try and figure out how to make teams. This bot had some issues with it which I will get to later. With all this in mind
+          I decided to kill two birds with one stone so to speak. Build a Bot to learn Node, and to help us speed up in house game creation.
               <Divider hidden />
           <Header size='medium' inverted={context.inverted}>Initial Goals for the bot</Header>
           <Divider />
@@ -71,13 +71,13 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
               Make the bot user friendly
                 </List.Item>
             <List.List>
-              Users should have direct interation with the team generation, or manually making teams shouldn't have to go thought the bot.
+              Users should have direct interaction with the team generation, or manually making teams shouldn't have to go thought the bot.
                 </List.List>
             <List.Item>
               Make the bot require less set up
                 </List.Item>
             <List.List>
-              With my old script I had to maintain my own list of what users would like to play as well as manually add/remove people 
+              With my old script, I had to maintain my own list of what users would like to play as well as manually add/remove people 
               from selection depending on who was playing. I want to be lazy too.
                 </List.List>
           </List>
@@ -91,10 +91,10 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
           is the other main include for this project, there's not much to say about it other then it's a nice api to use to build any sort of discord bot using JavaScript.
           <Divider hidden/>
           For those who are not familier with games. The idea of what can make a balanced game
-          is a very complex system, but in our case we can boil it down to say our goal is to get the closer we get the teams to a 50% chance to win, the better the balance.
+          is a very complex system, but in our case we can boil it down to say our goal is to get the teams as close to a 50% chance to win as possible, which reflects a better balance.
           Well how do we know what chance a team has to win? Enter the Elo system. Long story short Elo came up with a system to rank chess players. The higher the score the 
-          better the player and you can figure out the chance of winning each player has based on the Elo differance. We can use this same concept to say okay a team's Elo is
-          the sum of the Elo of the members and the chance of each team to win to be based on the differance in the teams Elo.
+          better the player and you can figure out the chance of winning each player has based on the Elo differance. We can use this same concept to say, okay a team's Elo is
+          the sum of the Elo of the members and the chance of each team to win is based on the differance in the teams Elo score.
           <Divider hidden/>
           Okay, so now we have a bot up and running, enhancements from my old bot, and a basic backings on how to make a team now to figure out the work flow. 
           <List ordered inverted={context.inverted}>
@@ -110,7 +110,7 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
               </List.List>
             </List.Item>
             <List.Item>
-              divide up the teams so they are even.... realize this doesn't seem obvious found out it's horribly bad to implemnt, find out it's something called The Partition Problem,
+              divide up the teams so they are even.... realize this doesn't seem obvious found out it's horribly bad to implement, find out it's something called The Partition Problem,
               realize that your flawed approach is the best approximation that doesn't suck, and move on
             </List.Item>
             <List.Item>
@@ -120,14 +120,14 @@ const DiscordBot: FunctionComponent<discordBotProps> = () => {
               Wait for the game to end by listening for a message
             </List.Item>
             <List.Item>
-              move the users back and update there Elo
+              move the users back and update their Elo
             </List.Item>
           </List>
           <Divider hidden/>
           Most of this is not exactly the most complex logic once you know what the flow is, but coming from Java it was interesting to learn about the event architecture. The two entry points
-          Where a user would start the process come both from a Discord message. Coming from a rest based enviroment I'd assume the API would work by hitting some http endpoint, But after using
+          Where a user would start the process both come from a Discord message. Coming from a rest-based enviroment I'd assume the API would work by hitting some http endpoint, But after using
           it a bit, it has it's pros and cons over this approach. What's nice is each event can be listed by many subscribers. One nice use case of this is the bot status card on this site.
-          With one event I can have every listener tied to a socket get an update pushed to it. In the case of the bot where I really only want one listener to execute this does mean we have a
+          With one event I can have every listener tied to a socket get an update pushed to it. In the case of the bot, in which I really only want one listener to execute, this does mean we have a
           kind of clunky if check or "guard clause" at the top of every event.
           <Divider hidden/>
           On top of events there is also learning about promises since so much of the interaction is between a discord server which obviously has a lot of time waiting for a response. (See what I did there?)
--- src/componets/content/welcome.tsx
+++ src/componets/content/welcome.tsx
@@ -44,7 +44,7 @@ const Welcome = () =>{
       {isMobile && ref ? renderDiscordBot() : <Divider hidden />}
       <Container text>
         The bot status updates in real time based on what I am doing, or what ever music my friends are playing in discord. This is done via web-sockets instead of http
-        and if you want to see the implimentation details both the bot repo that provides the stats as well as the repo used to display the bot status. Though the 
+        and if you want to see the implementation details both the bot repo that provides the stats as well as the repo used to display the bot status allow that, though the 
         backend has a lot of different integration points.
       </Container>
       <Divider hidden/>
